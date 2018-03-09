a700e28509068bf8ac4907872c5425862061599b
Vijayalakshmi Vedantham <vijimay12@gmail.com>
2018-03-10 02:59:30 +0530

    Backend complete

--- trial/hilocal/resources.py
+++ trial/hilocal/resources.py
@@ -1,16 +1,17 @@
 from tastypie.resources import ModelResource
 from hilocal.models import User
 from hilocal.models import Event
+from tastypie.authorization import Authorization
 
 class UserResource(ModelResource):
     class Meta:
         queryset = User.objects.all()
         resource_name = 'user'
-        #authorization = Authorization()
+        authorization = Authorization()
 
 
 class EventResource(ModelResource):
     class Meta:
         queryset = Event.objects.all()
         resource_name = 'event'
-        #authorization = Authorization()
+        authorization = Authorization()
