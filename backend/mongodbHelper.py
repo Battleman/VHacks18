c25dbd13eac6d25e3a701579041c570bb17b17ee
Olivier Cloux <olivier.cloux@epfl.ch>
2018-03-10 08:31:53 +0100

    modified gitignore and cleaning

--- backend/mongodbHelper.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-
-class mongoDB:
-    def __init__(self):
-        self.client = MongoClient("mongodb+srv://Admin:Admin@cluster0-wnxdp.mongodb.net/test")
-        self.db = self.client.business
-
